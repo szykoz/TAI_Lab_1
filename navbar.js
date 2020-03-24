040283f36f9c88416e02806479bf18aefdb3da74
szykoz <szykoz@outlook.com>
2020-03-24 22:48:29 +0100

    navbar component

---
+++ navbar.js
@@ -0,0 +1,10 @@
+let nav = '<nav class="nav">'+
+'<a class="nav-link" href="index.html">Home</a>'+
+'<a class="nav-link" href="quiz.html">Quiz</a>'+
+'<a class="nav-link" href="contact.html">Contact</a>'+
+'</nav>';
+
+
+let elem = document.getElementById("navbar");
+
+elem.innerHTML=nav;
